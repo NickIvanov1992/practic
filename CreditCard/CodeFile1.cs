33a37843fbb7cec18a533960c7b624fab295c8d0
NickIvanov <nikolas-bmx@yandex.ru>
2022-10-09 18:47:42 +0400

    готовое дз

---
+++ CreditCard/CodeFile1.cs
@@ -0,0 +1,19 @@
+﻿CreditCard card = new CreditCard();
+Client client = new Client();
+card.delegateRegister(client.Method1);                                             // делегат
+card.Put(50);
+card.Get(150);
+
+card.delegateRegister(delegate (string str) { Console.WriteLine(str); });      //аноним метод
+card.Put(200);
+card.Get(300);
+
+card.delegateRegister(str => Console.WriteLine(str));                      // лямбда
+card.Put(400);
+card.Get(5000);
+
+
+card.action += client.Method1;                                            // событие
+card.Put(10);
+card.Get(1000000);
+
---
+++ CreditCard/CreditCard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+    public class CreditCard
+    {
+        public delegate void AccountAction(string str);
+        AccountAction? givemoney;
+        public event AccountAction? action;
+
+        public void delegateRegister(AccountAction del) => givemoney = del;
+
+        public void Put(int money)
+        {
+           
+            givemoney?.Invoke($"Положить деньги :{money}");
+            action?.Invoke($"Событие Положить деньги  :{money}");
+        }
+
+        public void Get(int money)
+        {
+            
+                givemoney?.Invoke($"Снять деньги :{money}");
+                action?.Invoke($"Событие: Снять деньги :{money}");
+    }
+           
+        }
+    
+
