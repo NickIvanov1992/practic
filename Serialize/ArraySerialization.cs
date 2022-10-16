69a032b380ed6c04e63638b1ed7feda08989c717
Nick Ivanov <110496111+NickIvanov1992@users.noreply.github.com>
2022-10-16 23:09:48 +0400

    Serializing_class_Shape
    Xml and Json Serialize practic

---
+++ Serialize/ArraySerialization.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+
+
+public class ArraySerialization
+    {
+        public static void getserialize( Shape[] myArray)
+        {
+            Shape[]? desShape = null;
+            Console.WriteLine("введите формат сериализации: json или xml");
+            string? str = Console.ReadLine();
+
+            if (str == "json")
+            {
+                Console.WriteLine("Введите имя файла сериализации");
+                string? variable = Console.ReadLine();
+            try
+            {
+                using (Stream stream = new FileStream(variable + ".json", FileMode.OpenOrCreate))
+                {
+                    JsonSerializer.Serialize<Shape[]>(stream, myArray);
+                }
+
+                Console.WriteLine("Введите имя файла десериализации");
+                variable = Console.ReadLine();
+                using (Stream stream = new FileStream(variable + ".json", FileMode.OpenOrCreate))
+                {
+                    desShape = JsonSerializer.Deserialize<Shape[]>(stream);
+                }
+            }
+            catch(FormatException)
+            {
+               Console.WriteLine("Ошибка формата данных");
+            } 
+            }
+            if (str == "xml")
+            {
+            Console.WriteLine("Введите имя файла сериализации");
+            string? variable = Console.ReadLine();
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Shape[]));
+                using (Stream stream = new FileStream(variable + ".xml", FileMode.OpenOrCreate))
+                {
+                    xmlSerializer.Serialize(stream, myArray.ToArray());
+                }
+
+                Console.WriteLine("Введите имя файла десериализации");
+                variable = Console.ReadLine();
+                using (Stream stream = new FileStream(variable + ".xml", FileMode.OpenOrCreate))
+                {
+                    desShape = (Shape[]?)xmlSerializer.Deserialize(stream);
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Ошибка формата данных");
+            }
+            }
+        // Newtonsoft.Json
+
+        Console.WriteLine("Выполняется сериализация с пакетом Newtonsoft.Json ");
+        string newtonsoft_str = Newtonsoft.Json.JsonConvert.SerializeObject(myArray);
+        desShape = Newtonsoft.Json.JsonConvert.DeserializeObject<Shape[]>(newtonsoft_str);
+    }
+}
+
---
+++ Serialize/Program.cs
@@ -0,0 +1,22 @@
+﻿///Написать программу выполняющую сериализацию и десериализацию. 
+///Создать класс Point со свойствами float X, float Y. 
+///Создать класс Shape со свойствами Point ShapePoint, float Length, float Height, string Name. 
+///В методе Main создать массив из заполненных элементов типа Shape. 
+///Выполнить сериализацию и десериализацию этого массива. 
+///Название файла для сериализации и десериализации вводить с клавиатуры. 
+///Сериализуемые форматы XML и JSON. 
+///Так же необходимо использовать блоки для обработки исключений.
+///После выполнения задания, скачать nuget-пакет Newtonsoft.Json. 
+///Попробовать выполнить сериализацию с помощью Json сериализатора из этого пакета
+
+ArraySerialization arraySerialization = new ArraySerialization();
+Shape[] myArray = new Shape[2] 
+{
+    new Shape(new Point(10,20),30,40,"shape1"),
+    new Shape (new Point(50,60),70,80,"shape2")
+};
+
+ArraySerialization.getserialize(myArray);
+
+  
+    
