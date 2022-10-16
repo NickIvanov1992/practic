e04a30d29d0e5feba9ef1e25e8c3e29e7043e91e
NickIvanov <nikolas-bmx@yandex.ru>
2022-10-16 22:14:25 +0400

    сериализация_объекта_Shape

---
+++ Serialize/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Point
+    {
+    public Point(float x,float y)
+    {
+        
+    }
+    public Point()
+    {
+
+    }
+    public string? x { get; set; }
+    public string? y { get; set; }
+    }
+
---
+++ Serialize/Shape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Shape
+{
+    public Point ShapePoint { get; set; }
+    public float? Length { get; set; }
+    public float? Height { get; set; }
+    public string? Name { get; set; }
+    // конструктор с параметрами
+    public Shape(Point shapePoint, float length, float height, string name)
+    {
+        ShapePoint = shapePoint;
+        Length = length;
+        Height = height;
+        Name = name = "figure";
+    }
+    //конструктор без параметров для xml serialize
+    public Shape()
+    {
+
+    }
+ 
+}
+
